3fdd168b3cae6ef7a7d0e5b1f55520bd516b194a
sahinmaral <sahin.maral@hotmail.com>
2023-07-07 12:41:19 +0300

    Added All Idempotents

---
+++ basics/BookDemoWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using BookDemoWebAPI.Data;
+using BookDemoWebAPI.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(InMemoryContext.Books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById([FromRoute] int id)
+        {
+            var foundProduct = InMemoryContext.Books.SingleOrDefault(x => x.Id == id);
+            if (foundProduct is null) return NotFound($"Book with {id} ID could not found");
+            return Ok(foundProduct);
+        }
+
+        [HttpPost]
+        public IActionResult InsertBook([FromBody] Book book)
+        {
+            if (book.Title.Length == 0) return BadRequest("Title of new book must be filled");
+
+            Book existedBook = InMemoryContext.Books.SingleOrDefault(x => x.Title.Equals(book.Title));
+            if (existedBook is not null) return BadRequest($"Book with {book.Title} has already added");
+
+            InMemoryContext.Books.Add(book);
+            return Ok($"Book with {book.Title} has been added");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook([FromRoute]int id,[FromBody]Book book)
+        {
+            var foundProduct = InMemoryContext.Books.SingleOrDefault(x => x.Id == id);
+            if (foundProduct is null) return NotFound($"Book with {id} ID could not found");
+
+            foundProduct.Title = book.Title;
+            foundProduct.Price = book.Price;
+            return Ok(foundProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook([FromRoute] int id)
+        {
+            var foundProduct = InMemoryContext.Books.SingleOrDefault(x => x.Id == id);
+            if (foundProduct is null) return NotFound($"Book with {id} ID could not found");
+
+            InMemoryContext.Books.Remove(foundProduct);
+            return Ok(foundProduct);
+        }
+
+    }
+}
---
+++ basics/BookDemoWebAPI/Data/InMemoryContext.cs
@@ -0,0 +1,18 @@
+﻿using BookDemoWebAPI.Models;
+
+namespace BookDemoWebAPI.Data
+{
+    public static class InMemoryContext
+    {
+        public static List<Book> Books { get; set; }
+        static InMemoryContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book() {Id = 1,Title = "Karagoz ve Hacivat",Price= 75},
+                new Book() {Id = 2,Title = "Mesnevi",Price= 150},
+                new Book() {Id = 3,Title = "Dede Korkut",Price= 75},
+            };
+        }
+    }
+}
